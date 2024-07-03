36da808c3aecc74125d43e0eb241310d13dbad95
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2024-07-03 19:52:54 -0300

    feat: get endpoint to return json data

--- main.go
+++ main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
+var PORT = 8080
+
+var data = map[string]string{
+	"Go":     "A programming language created by Google.",
+	"Gopher": "A software engineer who builds with Go.",
+	"Golang": "Another name for Go.",
+}
+
 func helloHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Hello, you've requested: %s\n", req.URL.Path)
+	fmt.Fprintf(w, "Hello!")
+}
+
+func getData(w http.ResponseWriter, req *http.Request) {
+	// Sets up the Content-Type header so that the client knows to expect a JSON response
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
 }
 
 func main() {
-	// Define a simple handler function
+	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/get_data", getData)
 
-	// Register the handler function for a specific route pattern
-	http.HandleFunc("/hello", helloHandler)
+	fmt.Println("Server is listening on port " + strconv.Itoa(PORT))
 
-	// Start the HTTP server on port 8080
-	fmt.Println("Server is listening on port 8080...")
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":"+strconv.Itoa(PORT), nil)
 	if err != nil {
 		fmt.Printf("Error starting server: %s\n", err)
 	}
